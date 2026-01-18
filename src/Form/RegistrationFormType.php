31c046aed8804f94ab67077ad10b159f93634819
Harry7986 <emarh.harry.code@gmail.com>
2026-01-18 22:49:04 +0100

    Registration Form Type 7

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -41,32 +42,32 @@ class RegistrationFormType extends AbstractType
             ])
 
             ->add('plainPassword', RepeatedType::class, [
-    'type' => PasswordType::class,
-    'mapped' => false,
-    'first_options' => [
-        'label' => 'Mot de passe',
-        'attr' => [
-            'autocomplete' => 'new-password',
-        ],
-    ],
-    'second_options' => [
-        'label' => 'Confirmer le mot de passe',
-        'attr' => [
-            'autocomplete' => 'new-password',
-        ],
-    ],
-    'invalid_message' => 'Les mots de passe ne correspondent pas.',
-    'constraints' => [
-        new NotBlank(
-            message: 'Veuillez entrer un mot de passe.'
-        ),
-        new Length(
-            min: 6,
-            minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
-            max: 4096
-        ),
-    ],
-])
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Veuillez entrer un mot de passe.'
+                    ),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
+                        max: 4096
+                    ),
+                ],
+            ])
 
         ;
     }
