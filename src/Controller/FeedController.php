aa70ab64d711ab783511cb4b0daaec93d7d09d3c
Harry7986 <emarh.harry.code@gmail.com>
2026-01-18 23:55:05 +0100

    Feed Controller 2

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class FeedController extends AbstractController
+class FeedController extends AbstractController
 {
     #[Route('/feed', name: 'app_feed')]
     public function index(): Response
     {
-        return $this->render('feed/index.html.twig', [
-            'controller_name' => 'FeedController',
-        ]);
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        return $this->render('feed/index.html.twig');
     }
 }
