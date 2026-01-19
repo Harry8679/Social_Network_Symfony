ca35723a79b427fa3b64d102ff22c58e487be89f
Harry7986 <emarh.harry.code@gmail.com>
2026-01-19 11:30:57 +0100

    Feed 2

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -2,26 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FeedController extends AbstractController
 {
     #[Route('/feed', name: 'app_feed')]
-public function index(EntityManagerInterface $em): Response
-{
-    $this->denyAccessUnlessGranted('ROLE_USER');
+    public function index(EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-    $posts = $em->getRepository(Post::class)
-        ->findBy([], ['createdAt' => 'DESC']);
+        $posts = $em->getRepository(Post::class)
+            ->findBy([], ['createdAt' => 'DESC']);
 
-    $form = $this->createForm(PostType::class);
+        $form = $this->createForm(PostType::class);
 
-    return $this->render('feed/index.html.twig', [
-        'posts' => $posts,
-        'postForm' => $form->createView(),
-    ]);
-}
+        return $this->render('feed/index.html.twig', [
+            'posts' => $posts,
+            'postForm' => $form->createView(),
+        ]);
+    }
 
 }
